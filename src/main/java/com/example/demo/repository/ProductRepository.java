02c16b25f763ab2150a34e1bd0d913c5915af43e
Davit Hakobyan <davit@innorise.com>
2021-08-31 10:27:10 +0400

    add

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,13 +1,22 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Product;
-import lombok.Builder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ProductRepository  extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("select p from Product p where p.mPrice >= :lowerPrice and p.mPrice <= :upperPrice")
     List<Product> productsWithThisRange(int lowerPrice, int upperPrice);
+
+    @Query("SELECT u FROM Product u WHERE u.id = ?1")
+    Product getById(@Param("id") Long id);
+
+    @Query("select u from  Product u ")
+    List<Product> findAll();
+
+    @Query("Delete from Product where id = ?1")
+    void deleteCategoryById(Long id);
 }
